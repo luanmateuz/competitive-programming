a05bbee65a5dd0b8f1a66273c6bad58e9be11622
luanmateuzz <luanmateuzz@gmail.com>
2023-01-24 20:39:59 -0300

    bee 1116 go

---
+++ beecrowd/go/bee1116.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var n int
+	var x, y float64
+
+	fmt.Scanf("%d", &n)
+
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%f", &x)
+		fmt.Scanf("%f", &y)
+
+		if y != 0 {
+			fmt.Printf("%.1f\n", (x / y))
+		} else {
+			fmt.Println("divisao impossivel")
+		}
+
+	}
+
+}
